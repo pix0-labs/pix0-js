3bb8f7230f8fdc518bb9961d213e6e2b34417039
ketyung <ketyung@techchee.com>
2023-03-01 14:43:54 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './handlers/ins';
 export * from './handlers/query';
 export * from './models';
+export * from './utils';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,31 +1,10 @@
-const fs = require('fs');
-const os = require('os');
-require('dotenv').config();
-
 export interface Wallet {
-
+    
     mnemonic : string, 
+
     address : string, 
 }
 
-export const extractWallet = () : Wallet|undefined =>{
-
-    const homeDir = os.homedir();
-    const filePath = `${homeDir}/wallet.json`;
-    
-
-    let data = fs.readFileSync(filePath);
-
-    let wallet : Wallet|undefined = JSON.parse(data.toString());
-
-
-    return wallet;
-}
-
-export const extractWalletFromEnv = () : Wallet =>{
-
-    return { mnemonic : process.env.WALLET_MNEMONIC, address : process.env.WALLET_ADDRESS};
-}
 
 
 export const randomNumber = (min : number, max : number ): number =>{
