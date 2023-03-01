62a70dda833c59dea7d3344bbcce4d4635489754
ketyung <ketyung@techchee.com>
2023-03-01 12:02:43 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import * as ins from './handlers/ins';
-import * as query from './handlers/query';
-
-module.exports = {
-    createCollection : ins.createCollection,
-    createItem: ins.createItem,
-    getCollections: query.getCollections,
-    getItems: query.getItems,
-    getItemsCount : query.getItemsCount,
-};
+export * from './handlers/ins';
+export * from './handlers/query';
+export * from './models';
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    // some loaders...
+};
