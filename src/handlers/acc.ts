4b2e6b15916ff464f5f96249a9574faf4232e911
techchee <ketyung@techchee.com>
2023-03-04 17:55:40 +0800

    update

---
+++ src/handlers/acc.ts
@@ -0,0 +1,17 @@
+
+
+
+export const getAddressBalance = async  (address : string, client : any, coinMinimalDenom : string )
+: Promise<number|undefined> =>{
+    try {
+      let balance = await client.getBalance(address, coinMinimalDenom);
+     
+      return balance;
+
+    } 
+    catch (e : any ) {
+    
+        console.warn('Error reading account balance', [e, address]);
+    
+    }
+  };
