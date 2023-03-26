0971cdbb9c408a0b5020d8378a93795f01867c11
techchee <ketyung@techchee.com>
2023-03-26 08:56:16 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
-export const COLLECTION_CONTRACT_ADDR = "archway1c370kt54c93ex2j6pfc6fsc2ws57kqjrlcjl38ylgw2twxnt49hsuv6qwp";
+export const COLLECTION_CONTRACT_ADDR = "archway1m34cg2y0w027p9jkpa590seq0u3n79x9yjcdl44e5ff8468xc7sqt796hr";
+
+//"archway1c370kt54c93ex2j6pfc6fsc2ws57kqjrlcjl38ylgw2twxnt49hsuv6qwp";
 
 //"archway1gcx20hxgljksuumngvjg75swagn20sst5vf9k65uc2xjms9ntkzs23hzud";
 
