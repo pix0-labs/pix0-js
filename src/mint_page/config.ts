31ff9f868e3f011401f634deb252bfa1f1a97523
techchee <ketyung@techchee.com>
2023-05-04 11:22:44 +0800

    update

--- src/mint_page/config.ts
+++ src/mint_page/config.ts
@@ -1,4 +1,6 @@
-export const MINT_PAGE_CONTRACT_ADDR= "archway1n8fne080zj8ajlsty4c4un83rcjhqwskntk45sqjjapk3qhtee5qmg6lwt";
+export const MINT_PAGE_CONTRACT_ADDR = "archway16jw8vcxd2f6fyxnq6cv6pvzwrrstvt8zz2hj43v4yyx84ur2kusq86pykn";
+
+//"archway1n8fne080zj8ajlsty4c4un83rcjhqwskntk45sqjjapk3qhtee5qmg6lwt";
 
 //"archway1g2w2fwz2t70mts3jrcs39u9fss5q90n3ns0a8zlmp0m9yq59lwms46kqzg";
 
